fde8c706fba7fb33c1b0be3b457184bb31f7203c
TheElectricShekel <79342142+TheElectricShekel@users.noreply.github.com>
2021-05-06 21:05:19 -0400

    v1.02
    -re-wrote the bot to scrape the links using BeautifulSoup instead of relying on xpaths

--- publish0x_bot.py
+++ publish0x_bot.py
@@ -2,6 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementNotInteractableException
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+from IPython.display import HTML
 from PIL import Image
 from io import BytesIO
 import time
@@ -9,258 +14,127 @@ import pytesseract
 import pyperclip
 import keyboard
 import sys
+import os
 from random import randrange
 
 
+def slider_main():
+    slider = browser.find_element_by_id("tipslider")
+    # browser.execute_script('window.scrollBy(0, 5000)')  # x=0, y=500
+    browser.execute_script("arguments[0].scrollIntoView();", slider)
+    print("Scrolled to tip slider")
+
+    time.sleep(2)
+    try:
+        slider.click()
+    except ElementNotInteractableException:
+        print("Element not interactable")
+        for remaining in range(300, 0, -1):
+            sys.stdout.write("\r")
+            sys.stdout.write("Tip cooldown #2: {:2d} seconds remaining.".format(remaining))
+            sys.stdout.flush()
+            time.sleep(1)
+        sys.stdout.write("\rChecking for tip slider...            \n")
+        slider.click()
+
+    i = 60
+
+    while i > 0:
+        slider.send_keys(Keys.ARROW_LEFT)
+        i -= 1
+
+    print("Tip Slider moved left")
+    print(">happymerchant.jpg")
+
+
+article_num = 0
+ref_link = "?a=WZdP1yqAeK"  # Leave it in to say thanks :)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+linktxt = "links.txt"
 driver_path = "chromedriver.exe"
 brave_path = "C:/Program Files/Google/Chrome/Application/chrome.exe"
 
+r = urllib.request.urlopen('https://www.publish0x.com/newposts').read()
+soup = BeautifulSoup(r, "lxml")
+type(soup)
+
+first_time = 1
+
+with open(linktxt, 'a') as f:
+    f.truncate(0)
+    for link in soup.find_all('a', {'class': "blogname"}):
+        f.write(link.get('href') + '\n')
+        print(link.get('href'))
+
+with open(linktxt) as f:
+    link_list = f.readlines()
+
 option = webdriver.ChromeOptions()
 option.binary_location = brave_path
-#option.add_argument("--incognito")
-#option.add_argument("--headless")
 
-ref_link = "?a=WZdP1yqAeK"
-scroll_pause_time = 0.5
-
-# Create new Instance of Chrome
 browser = webdriver.Chrome(executable_path=driver_path, chrome_options=option)
-print("Browser launched")
-
 browser.get("https://www.publish0x.com/login")
-
-input("Press Enter to continue...")
+print("Browser launched")
 
 time.sleep(1)
 
-timer_count = 0
+#username = browser.find_element_by_xpath("/html/body/div[2]/main/div/div[2]/div/form/div[1]/div/input")
+#username.send_keys("")
+#password = browser.find_element_by_xpath("/html/body/div[2]/main/div/div[2]/div/form/div[2]/div/input")
+#password.send_keys("")
 
-xpath_1 = "/html/body/div[2]/main/div/div[1]/div["
-article_num = 1
-xpath_2 = "]/div[1]/h2/a"
+input("Press Enter to continue...")
 
-while timer_count < 10:
-    browser.get("https://www.publish0x.com/newposts")
-    print("Navigated to New Posts")
-
-    while article_num < 10:
-        try:
-            print("Checking for ad overlay")
-            ad_check = browser.find_element_by_xpath("/html/body/div[5]/span")
-            ad_check.click()
-            print("Ads closed")
-        except NoSuchElementException:
-            print("No Ads found")
-            #pass
+while article_num < 11:
+    article_url = "https://www.publish0x.com" + link_list[article_num] + ref_link
+    browser.get(article_url)
 
-        try:
-            article_xpath = xpath_1+str(article_num)+xpath_2
-            article = browser.find_element_by_xpath(article_xpath)
-            article.click()
-            article_click = "Clicked article #"
-            print(article_click+str(article_num))
-
-            time.sleep(2)
-
-            url = browser.current_url
-            ref_url = url + ref_link
-            print("URL amended: "+ref_url)
-            browser.get(ref_url)
-            time.sleep(2)
-
-        except NoSuchElementException:
-            browser.get("https://www.publish0x.com/newposts")
+    if first_time != 1:
+        for remaining in range(605, 0, -1):
+            sys.stdout.write("\r")
+            sys.stdout.write("Tip cooldown: {:2d} seconds remaining.".format(remaining))
+            sys.stdout.flush()
             time.sleep(1)
-            #browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            #view_more = browser.find_element_by_xpath("/html/body/div[3]/main/div/div[2]/div/button")
-            #view_more.click()
-            browser.execute_script("window.scrollTo(0, 1080)")
+        sys.stdout.write("\rChecking for tip slider...            \n")
 
-            time.sleep(3)
+    try:
+        slider_main()
+    except:
+        time.sleep(120)
+        print("Timer not complete")
+        slider_main()
 
-            article_xpath = xpath_1 + str(article_num) + xpath_2
-            article = browser.find_element_by_xpath(article_xpath)
-            article.click()
-            article_click = "Clicked article #"
-            print(article_click + str(article_num))
+    time.sleep(1)
 
-            time.sleep(2)
-
-            url = browser.current_url
-            ref_url = url + ref_link
-            print("URL amended: " + ref_url)
-            browser.get(ref_url)
-            time.sleep(2)
-
+    try:
+        tip_button = browser.find_element_by_xpath("/html/body/div[2]/main/div/div[8]/div/div/div/div/div[3]/button")
+        tip_button.click()
+    except:
         try:
-            slider = browser.find_element_by_id("tipslider")
-            #browser.execute_script('window.scrollBy(0, 5000)')  # x=0, y=500
-            browser.execute_script("arguments[0].scrollIntoView();", slider)
-
-            print("Scrolled to tip slider")
-
-            time.sleep(2)
-
-            slider.click()
-            i = 60
-
-            while i > 0:
-                slider.send_keys(Keys.ARROW_LEFT)
-                i -= 1
-
-            print("Tip Slider moved left")
-            print(">happymerchant.jpg")
-
-            time.sleep(1)
-            try:
-                tip_button = browser.find_element_by_xpath("/html/body/div[2]/main/div/div[8]/div/div/div/div/div[3]/button")
-                tip_button.click()/html/body/div[2]/main/div/div[8]/div/div[1]/form/div/div[3]/button
-            except:
-                tip_button = browser.find_element_by_xpath(
-                    "/html/body/div[2]/main/div/div[8]/div/div[1]/form/div/div[3]/button")
-                tip_button.click()
-
-            print("Tip submitted")
-            timer_count = 0
-
-            if article_num > 1:
-                articles_read = " articles read"
-            else:
-                articles_read = " article read"
-
-            print(str(article_num)+articles_read)
-
-            article_num += 1
-
-            print("Waiting for tip reset:")
-
-            for remaining in range(600, 0, -1):
-                sys.stdout.write("\r")
-                sys.stdout.write("{:2d} seconds remaining.".format(remaining))
-                sys.stdout.flush()
-                time.sleep(1)
-            sys.stdout.write("\rComplete!            \n")
-
+            tip_button = browser.find_element_by_xpath(
+                "/html/body/div[2]/main/div/div[8]/div/div[1]/form/div/div[3]/button")
+            tip_button.click()
         except:
-            print("Timer found on page")
-            print("Next article")
-            article_num += 1
-            timer_count += 1
-            if timer_count == 9:
-                time.sleep(600)
-            pass
+            tip_button = browser.find_element_by_xpath(
+                "/html/body/div[2]/main/div/div[8]/div/div[2]/form/div/div[3]/button")
+            tip_button.click()
 
-    while article_num >= 10:
-        try:
-            print("Checking for ad overlay")
-            ad_check = browser.find_element_by_xpath("/html/body/div[5]/span")
-            ad_check.click()
-            print("Ads closed")
-        except NoSuchElementException:
-            print("No Ads found")
-            # pass
+    print("Tip submitted")
 
-        try:
-            #browser.execute_script("window.scrollTo(0, 1080)")
+    article_num = article_num + 1
 
-            article_xpath = xpath_1 + str(article_num) + xpath_2
-            article = browser.find_element_by_xpath(article_xpath)
-            article.click()
-            article_click = "Clicked article #"
-            print(article_click + str(article_num))
+    first_time = 0
 
-            time.sleep(2)
+    #for remaining in range(600, 0, -1):
+    #    sys.stdout.write("\r")
+    #    sys.stdout.write("Tip cooldown: {:2d} seconds remaining.".format(remaining))
+    #    sys.stdout.flush()
+    #    time.sleep(1)
 
-            url = browser.current_url
-            ref_url = url + ref_link
-            print("URL amended: " + ref_url)
-            browser.get(ref_url)
-            time.sleep(2)
 
-        except NoSuchElementException:
-            #view_more = browser.find_element_by_xpath("/html/body/div[2]/main/div/div[2]/div/button")
-            #view_more.click()
-            browser.execute_script("window.scrollTo(0, 1080)")
 
-            time.sleep(3)
 
-            article_xpath = xpath_1 + str(article_num) + xpath_2
-            article = browser.find_element_by_xpath(article_xpath)
-            article.click()
-            article_click = "Clicked article #"
-            print(article_click + str(article_num))
-
-            time.sleep(2)
-
-            url = browser.current_url
-            ref_url = url + ref_link
-            print("URL amended: " + ref_url)
-            browser.get(ref_url)
-            time.sleep(2)
-
-        try:
-            slider = browser.find_element_by_id("tipslider")
-            # browser.execute_script('window.scrollBy(0, 5000)')  # x=0, y=500
-            browser.execute_script("arguments[0].scrollIntoView();", slider)
-
-            print("Scrolled to tip slider")
-
-            time.sleep(2)
-
-            slider.click()
-            i = 60
-
-            while i > 0:
-                slider.send_keys(Keys.ARROW_LEFT)
-                i -= 1
-
-            print("Tip Slider moved left")
-            print(">happymerchant.jpg")
-
-            time.sleep(1)
-
-            try:
-                tip_button = browser.find_element_by_xpath("/html/body/div[2]/main/div/div[8]/div/div/div/div/div[3]/button")
-                tip_button.click()/html/body/div[2]/main/div/div[8]/div/div[1]/form/div/div[3]/button
-            except:
-                tip_button = browser.find_element_by_xpath(
-                    "/html/body/div[2]/main/div/div[8]/div/div[1]/form/div/div[3]/button")
-                tip_button.click()
-
-            print("Tip submitted")
-            timer_count = 0
-
-            if article_num > 1:
-                articles_read = " articles read"
-            else:
-                articles_read = " article read"
-
-            print(str(article_num) + articles_read)
-
-            article_num += 1
-
-            print("Waiting for tip reset:")
-
-            for remaining in range(600, 0, -1):
-                sys.stdout.write("\r")
-                sys.stdout.write("{:2d} seconds remaining.".format(remaining))
-                sys.stdout.flush()
-                time.sleep(1)
-            sys.stdout.write("\rComplete!            \n")
-
-        except:
-            print("Timer found on page")
-            print("Next article")
-            article_num += 1
-            timer_count += 1
-            if timer_count == 9:
-                time.sleep(600)
-            pass
-
-    #time.sleep(600)
-
-print("Maximum amount of daily tips given")
-print("Exit: Success")
 
 
 
